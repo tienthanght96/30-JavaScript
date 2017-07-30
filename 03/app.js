07dd34d1fcf80dc8beca75dcce6bf514a332824a
unknown <trantienthanght96@gmail.com>
2017-07-30 09:44:32 +0700

    edit spacing, blur, color default

--- 03/app.js
+++ 03/app.js
@@ -4,7 +4,7 @@ const listInput = document.querySelectorAll('.controls input');
 function updateEvent() {
     let suffix = this.dataset.type || '';
     document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
-    console.log(this.name + '-' + this.value)
+
 }
 
 listInput.forEach(
